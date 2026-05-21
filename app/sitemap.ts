6225e01460a5e0acc3e9588b013f75c2dc6681d1
faizaliali <139729561+faizaliali@users.noreply.github.com>
2026-05-21 12:48:56 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,30 @@
+import { MetadataRoute } from "next";
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.alamhandloomcarpets.in",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.alamhandloomcarpets.in/mohali-carpet-shop",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.alamhandloomcarpets.in/panchkula-carpet-shop",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.alamhandloomcarpets.in/zirakpur-carpet-shop",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+}
