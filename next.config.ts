0440aec69e1f01e14dd5bd33a2d948afcdd0d362
faizaliali <139729561+faizaliali@users.noreply.github.com>
2026-06-07 13:51:22 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/mohali-carpet-shop",
-        destination: "/carpet-shop-mohali",
-        permanent: true,
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
