f63ced70b08c4134682889a903eb179538b8b622
faizaliali <139729561+faizaliali@users.noreply.github.com>
2026-07-28 23:57:14 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -32,11 +32,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.8,
     },
-    {
-      url: "https://www.alamhandloomcarpets.in/mumbai-carpet-shop",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
   ];
 }
